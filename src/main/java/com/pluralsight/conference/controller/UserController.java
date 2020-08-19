a781528aff51f5802a366ea13831205de6f9d2b6
arokde <arokde@rocketsoftware.com>
2020-08-19 16:33:42 +0530

    Simple spring mvc project

---
+++ src/main/java/com/pluralsight/conference/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.pluralsight.conference.controller;
+
+import com.pluralsight.conference.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam(value = "firstname", defaultValue = "Amey") String firstName,
+                        @RequestParam(value = "lastname", defaultValue = "Rokde") String lastName,
+                        @RequestParam(value = "age", defaultValue = "10") int age) {
+        final User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setAge(age);
+        return user;
+    }
+
+
+    @PostMapping("/user")
+    public User addUser(User user) {
+        System.out.println("User first name" + user.getFirstName());
+        return user;
+    }
+}
